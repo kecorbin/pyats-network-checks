841238704c947ad908a9626d455dbf6695cff81c
Julio <juliogomezsanchez@gmail.com>
2019-04-05 19:54:38 +0200

    fix how to invoke it

--- bgp_adjacencies/BGP_check_job.py
+++ bgp_adjacencies/BGP_check_job.py
@@ -1,14 +1,14 @@
 # To run the job:
-# pyats run job BGP_check_job.py --testbed_file <testbed_file.yaml>
+# pyats run job BGP_check_job.py --testbed-file <testbed_file.yaml>
 # Description: This job file checks if all BGP neighbors are in the 'established' state
 import os
 from ats.easypy import run
 
-
 # All run() must be inside a main function
 def main():
     # Find the location of the script in relation to the job file
-    bgp_tests = os.path.join('./BGP_Neighbors_Established.py')
+    pwd = os.path.dirname(__file__)
+    bgp_tests = os.path.join(pwd, 'BGP_Neighbors_Established.py')
     # Execute the testscript
     # run(testscript=testscript)
     run(testscript=bgp_tests)
